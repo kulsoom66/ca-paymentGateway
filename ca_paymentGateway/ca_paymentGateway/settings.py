6dcd5b16bb2e60ea8b3ecff4e3c22bdfb4dfd7b9
kulsoom66 <kulsoomshoukat66@gmail.com>
2024-06-13 12:45:48 +0400

    changed in cancel.html and succes.html

--- ca_paymentGateway/ca_paymentGateway/settings.py
+++ ca_paymentGateway/ca_paymentGateway/settings.py
@@ -23,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kvxqzo13%@zd^nu)2zxl^pp$7!=70a71u5a7%hgf*e&t%dy!eq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
